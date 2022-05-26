c81d4e7fde1b81bff5246341c04009ffc37d68ae
chenwenyan <2372085181@qq.com>
2022-05-26 17:21:20 +0800

    创建导航层次结构

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct MeetingView: View {
-    var scrum = DailyScrum.sampleData[0]
     var body: some View {
         VStack {
             ProgressView(value: 5, total: 15)
@@ -32,7 +31,6 @@ struct MeetingView: View {
                 .strokeBorder(lineWidth: 24)
             HStack {
                 Text("Speaker 1 of 3")
-                    .foregroundColor(scrum.theme.mainColor)
                 Spacer()
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
