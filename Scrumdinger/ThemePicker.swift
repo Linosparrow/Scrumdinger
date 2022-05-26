f3b8bb218dcf67f6fb46e0e2522f020436439b57
chenwenyan <2372085181@qq.com>
2022-05-26 22:22:21 +0800

    使用绑定传递数据

---
+++ Scrumdinger/ThemePicker.swift
@@ -0,0 +1,27 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by 陈文岩 on 2022/5/26.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
